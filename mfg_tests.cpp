c1283e42fce727c5e971ffd952d2c45a6b089a65
Alexander Efremov <alexander.efremov@asposeptyltd.com>
2017-04-24 22:06:44 +0700

    Added L1 norm calcs

--- mfg_tests.cpp
+++ mfg_tests.cpp
@@ -25,17 +25,19 @@ void print_params(int n, int n_1, double a_coef, double sigma, double sigma_sq, 
     fflush(stdout);
 }
 
-void run_solver_1() {
+
+
+void run_solver_1(int _n, double _tau, int _time_step_cnt, double _sigma) {
     double a = 0.;
     double b = 1.;
     double a_coef = 1.;
-    int time_step_cnt = 1;
-    double tau = 1e-3;
-    double sigma = 1.;
+    int time_step_cnt = _time_step_cnt;
+    double tau = _tau;
+    double sigma = _sigma;
     double sigma_sq = sigma * sigma;
     double u = 2.;
 
-    int n = 20;
+    int n = _n;
     int n_1 = n + 1;
     double h = (b - a) / n;
     double h_2 = 0.5 * h;
@@ -53,121 +55,51 @@ void run_solver_1() {
     free(exact_m);
 }
 
+void run_solver_1() {
+    int time_step_cnt = 1;
+    double tau = 1e-3;
+    double sigma = 1.;
+    int n = 20;
+    run_solver_1(n, tau, time_step_cnt, sigma);
+}
+
 TEST_CASE("mfg2_solver_1", "[run_solver_1]") {
     run_solver_1();
 }
 
-//TEST_CASE("mfg_solver_1") {
-//    int exp_cnt = 4;
-//    double *l1_arr = (double *) malloc(exp_cnt * sizeof(double));
-//    for (int i = 0; i < exp_cnt; ++i) {
-//        printf("\n\n=============== EXPERIMENT %d ================ \n\n", i + 1);
-//        int nx = 0;
-//        double tau = 1e-3;
-//        double sigma = 1.;
-//        int tsc = 10;
-//        double mult = 1.;
-//        switch (i) {
-//            case 0:
-//                nx = 50;
-//                tau = tau / mult;
-//                tsc = 10 * (int) mult;
-//                break;
-//            case 1:
-//                mult = 4.;
-//                nx = 100;
-//                tau = tau / mult;
-//                tsc = 10 * (int) mult;
-//                break;
-//            case 2:
-//                mult = 16.;
-//                nx = 200;
-//                tau = tau / mult;
-//                tsc = 10 * (int) mult;
-//                break;
-//            case 3:
-//                mult = 32.;
-//                nx = 400;
-//                tau = tau / mult;
-//                tsc = 10 * (int) mult;
-//                break;
-//            case 4:
-//                mult = 64.;
-//                nx = 800;
-//                tau = tau / mult;
-//                tsc = 10 * (int) mult;
-//                break;
-//            case 5:
-//                mult = 128.;
-//                nx = 1600;
-//                tau = tau / mult;
-//                tsc = 10 * (int) mult;
-//                break;
-//            default:
-//                return;
-//        }
-//        printf("TAU * TIME_STEP_COUNT = %e", tau * tsc);
-//        assert(tau * tsc == 0.01);//0.01 sec
-//        double l1 = run_solver_1(nx, tau, tsc, sigma);
-//        l1_arr[i] = l1;
-//
-//    }
-//
-//    double l1_max = -10000000.;
-//    for (int j = 0; j < exp_cnt; ++j) {
-//        if (l1_arr[j] > l1_max)
-//            l1_max = l1_arr[j];
-//    }
-//    printf("\n\nL1 MAX ON EXPERIMENTS: %e\n", l1_max);
-//
-//    free(l1_arr);
-//}
-//
-//TEST_CASE("mfg_solver_1_tsc_const") {
-//    int exp_cnt = 4;
-//    double *l1_arr = (double *) malloc(exp_cnt * sizeof(double));
-//    for (int i = 0; i < exp_cnt; ++i) {
-//        printf("\n\n=============== EXPERIMENT %d ================ \n\n", i + 1);
-//        int nx = 0;
-//        double tau = 1e-3;
-//        double sigma = 1.;
-//        int tsc = 10;
-//        double mult = 1.;
-//        switch (i) {
-//            case 0:
-//                nx = 50;
-//                break;
-//            case 1:
-//                nx = 100;
-//                break;
-//            case 2:
-//                nx = 200;
-//                break;
-//            case 3:
-//                nx = 400;
-//                break;
-//            case 4:
-//                nx = 800;
-//                break;
-//            case 5:
-//                nx = 1600;
-//                break;
-//            default:
-//                return;
-//        }
-//        printf("TAU * TIME_STEP_COUNT = %e", tau * tsc);
-////        assert(tau * tsc == 0.01);//0.01 sec
-//        double l1 = run_solver_1(nx, tau, tsc, sigma);
-//        l1_arr[i] = l1;
-//    }
-//
-//    double l1_max = -10000000.;
-//    for (int j = 0; j < exp_cnt; ++j) {
-//        if (l1_arr[j] > l1_max)
-//            l1_max = l1_arr[j];
-//    }
-//    printf("\n\nL1 MAX ON EXPERIMENTS: %e\n", l1_max);
-//
-//    free(l1_arr);
-//}
+TEST_CASE("mfg_solver_1_tsc_const", "[run_solver_1_many]") {
+    int exp_cnt = 4;
+    for (int i = 0; i < exp_cnt; ++i) {
+        printf("\n\n=============== EXPERIMENT %d ================ \n\n", i + 1);
+        int nx = 0;
+        double tau = 1e-3;
+        double sigma = 1.;
+        int time_step_cnt = 10;
+        double mult = 1.;
+        switch (i) {
+            case 0:
+                nx = 50;
+                break;
+            case 1:
+                nx = 100;
+                break;
+            case 2:
+                nx = 200;
+                break;
+            case 3:
+                nx = 400;
+                break;
+            case 4:
+                nx = 800;
+                break;
+            case 5:
+                nx = 1600;
+                break;
+            default:
+                return;
+        }
+        printf("TAU * TIME_STEP_COUNT = %e", tau * time_step_cnt);
+        run_solver_1(nx, tau, time_step_cnt, sigma);
+    }
+}
 
--- solver1.cpp
+++ solver1.cpp
@@ -34,7 +34,7 @@ inline double func_alpha(double a_coef, double t, double x) {
 }
 
 inline double get_f(double sigma_sq, double a_coef, double x, double t, double u) {
-    return a_coef * ( x * (1. - x) + u * t * (1. - 2. * x) + 2. * sigma_sq * t );
+    return a_coef * (x * (1. - x) + u * t * (1. - 2. * x) + 2. * sigma_sq * t);
 }
 
 double analytical_solution_1(double a_coef, double t, double x) {
@@ -131,20 +131,27 @@ double *solve_1(int n, int n_1, double h, double h_sq, double h_2, double sigma_
                 double a_coef, double tau, int time_step_cnt, double u, double *exact_sol_to_fill) {
     assert_params(h, h_sq, h_2, sigma_sq, n, n_1, sigma, a, tau, a_coef, time_step_cnt);
 
+    char m_fout[25];
+
     double *m = (double *) malloc(n_1 * sizeof(double));
     double *m_pr = (double *) malloc(n_1 * sizeof(double));
+    double *ex_m = (double *) malloc(n * sizeof(double));
     double *rp = (double *) malloc(n_1 * sizeof(double));
     double *err = (double *) malloc(n_1 * sizeof(double));
     double *b_t = (double *) malloc(n_1 * sizeof(double));
     double *c_t = (double *) malloc(n_1 * sizeof(double));
     double *d_t = (double *) malloc(n_1 * sizeof(double));
 
+    double *max = (double *) malloc((time_step_cnt + 1) * sizeof(double));
+    double *l1_norm_err = (double *) malloc((time_step_cnt + 1) * sizeof(double));
+    double *l1_norm_sol = (double *) malloc((time_step_cnt + 1) * sizeof(double));
+
     fill_b(b_t, n_1, tau, sigma_sq);
     fill_c(c_t, n_1, tau, sigma_sq, h_sq);
     fill_d(d_t, n_1, tau, sigma_sq);
     print_thomas_arrays(b_t, c_t, d_t, n_1);
 
-    for (int i = 0; i < n_1; ++i) m[i] = rp[i] = m_pr[i] = 0.;
+    for (int i = 0; i < n_1; ++i) m[i] = m_pr[i] = rp[i] = 0.;
 
     fill_arr_by_ex_sol(m_pr, n_1, 0., a_coef, h, a);
 
@@ -154,16 +161,53 @@ double *solve_1(int n, int n_1, double h, double h_sq, double h_2, double sigma_
         fill_rp(rp, m_pr, tau * tl, n_1, tau, h, h_sq, sigma_sq, a_coef, a, b, u);
         thomas_algo_verzh(n_1, b_t, c_t, d_t, rp, m);
         memcpy(m_pr, m, n_1 * sizeof(double));
+
+        fill_arr_by_ex_sol(ex_m, n_1, tau * tl, a_coef, h, a);
+        fill_arr_diff(err, ex_m, m, n_1);
+        //print_vector("EXACT SOL", ex_m, n_1);
+        //print_vector("ERR", err, n_1);
+
+        max[tl] = get_max_fabs_array(err, n_1);
+        l1_norm_err[tl] = get_l1_norm(h, n_1, err);
+        l1_norm_sol[tl] = get_l1_norm(h, n_1, m);
+
+        printf("TIME LEVEL %d \n", tl);
+        printf("uniform norm %22.14le\n", max[tl]);
+        printf("l1 norm of error %22.14le\n", l1_norm_err[tl]);
+        printf("l1 norm of num solution%22.14le\n", l1_norm_sol[tl]);
     }
 
     fill_arr_by_ex_sol(exact_sol_to_fill, n_1, tau * time_step_cnt, a_coef, h, a);
     print_vector("EXACT", exact_sol_to_fill, n_1);
+
+    max[0] = get_max_fabs_array(l1_norm_err, time_step_cnt + 1);
+    printf("uniform norm of l1 norm of error %22.14le\n", max[0]);
+
+    sprintf(m_fout, "%f_%f_%s.out", tau * time_step_cnt, h, "err");
+    print_vector2D_to_file(n, a, h, err, m_fout);
+
+    sprintf(m_fout, "%f_%f_%s.out", tau * time_step_cnt, h, "m");
+    print_vector2D_to_file(n, a, h, m, m_fout);
+
+    fill_arr_by_ex_sol(ex_m, n, tau * time_step_cnt, a_coef, h, a);
+//    printf("EXACT SOL \n");
+//    print_matrix1(ex_m, 1, n);
+
+    fill_arr_diff(err, ex_m, m, n);
+//    printf("ERR \n");
+//    print_matrix1(err, 1, n);
+
+    fill_arr_by_ex_sol(exact_sol_to_fill, n, tau * time_step_cnt, a_coef, h, a);
 
     print_vector("APPROX", m, n_1);
 
     fill_arr_diff(err, exact_sol_to_fill, m, n_1);
     print_vector("ERR", err, n_1);
 
+    free(ex_m);
+    free(max);
+    free(l1_norm_err);
+    free(l1_norm_sol);
     free(m_pr);
     free(rp);
     free(err);
